b93d6e96af41a81c8c669e9ffc1faf696f0d9051
Granit-700 <114147712+Granit-700@users.noreply.github.com>
2025-09-18 18:27:55 +0600

    изменил импорт типов

--- src/componets/Nav/NavButton.tsx
+++ src/componets/Nav/NavButton.tsx
@@ -1,7 +1,7 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import type { Category } from "../../types";
+import type { Category } from "../../types/mainPage/mainPage";
 
 interface NavButtonProps {
   category: Category;
